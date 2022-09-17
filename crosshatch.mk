bffb0f015ae6847b91d635c565311b4028a06286
mattDev0 <abraham.odunayo.191009@fuoye.edu.ng>
2022-09-17 19:47:09 +0100

    Set fingerprint props properly

--- crosshatch.mk
+++ crosshatch.mk
@@ -25,10 +25,4 @@ PRODUCT_NAME := crosshatch
 PRODUCT_DEVICE := crosshatch
 PRODUCT_MODEL := Pixel 3 XL
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=crosshatch \
-    PRIVATE_BUILD_DESC="crosshatch-user 12 SP1A.210812.016.C2 8618562 release-keys"
-
-BUILD_FINGERPRINT := google/crosshatch/crosshatch:12/SP1A.210812.016.C2/8618562:user/release-keys
-
 
