19843012ea581d50eb73e4201c6a4634ea95964e
mattDev0 <abraham.odunayo.191009@fuoye.edu.ng>
2023-04-30 08:24:52 +0000

    build GoogleCamera

--- hentai_common.mk
+++ hentai_common.mk
@@ -56,6 +56,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
 
+# Camera
+PRODUCT_PACKAGES += \
+    GcamPrebuilt
+
 # Build Tensor/Pixel 2021 features
 PRODUCT_PACKAGES += \
     PixelWallpapers2021 \
