461297976cfefa2bea5ed5f80ae6c6723c5e772e
mattDev0 <abraham.odunayo.191009@fuoye.edu.ng>
2023-05-11 09:59:29 +0000

    b1c1: restore aosp_product.mk

--- statix_common.mk
+++ statix_common.mk
@@ -30,6 +30,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
+#
+# All components inherited here go to product image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
+
 # Inherit some common StatiX stuff.
 $(call inherit-product, vendor/statix/config/common.mk)
 $(call inherit-product, vendor/statix/config/gsm.mk)
