22a5186013f760d9f2e7714990abdd51c0e06aab
mattDev0 <abraham.odunayo.191009@fuoye.edu.ng>
2023-05-11 09:48:52 +0000

    switch to stock fingerprint

--- statix_crosshatch.mk
+++ statix_crosshatch.mk
@@ -28,3 +28,9 @@ PRODUCT_MODEL := Pixel 3 XL
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=crosshatch \
+    PRIVATE_BUILD_DESC="crosshatch-user 12 SP1A.210812.016.C2 8618562 release-keys"
+
+BUILD_FINGERPRINT := google/crosshatch/crosshatch:12/SP1A.210812.016.C2/8618562:user/release-keys
